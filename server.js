92045008d5e9996449c3c4b45afb821ef07350c9
ttngu <tammyn137@gmail.com>
2021-02-01 16:15:03 -0800

    updated mongo and deploying

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  process.env.MONGODB_URI || "mongodb+srv://fitness-admin:<harrypotter>@cluster0.ftenn.mongodb.net/<googlebooks>?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
     useNewUrlParser: true
